7c35fce8780b6e0f0400d5ca498c864cf772aa9d
Abdul Samad Robin <56694020+asrobin1052@users.noreply.github.com>
2021-05-27 21:30:11 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Fusion-Tech-2.0-Online-Shop
-This Is My First Website Project.
+
+
+# Installation
+
+1>Install XAMPP.
+
+2>Open XAMPP Control panal and start [apache] and [mysql] .
+
+3>extract files in C:\\xampp\htdocs\.
+
+4> open link localhost/phpmyadmin
+
+5>click on new at side navbar.
+
+6>give a database name as (fusion-tech) hit on create button.
+
+7>after creating database name click on import.
+
+8>first register and then login
+
+9>admin login details  Email=admin@gmail.com and Password=123456789.
+
+
+
---
+++ newslettter.php
@@ -0,0 +1,42 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Sign Up for the <strong>OFFER UPDATE'S</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+							</form>
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+							<ul class="newsletter-follow">
+								<li>
+									<a href="https://facebook.com/fusiontech2.0"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href="https://github.com/asrobin1052"><i class="fa fa-github"></i></a>
+								</li>
+								<li>
+									<a href="https://www.instagram.com/robin_ahmad_shuvo/"><i class="fa fa-instagram"></i></a>
+								</li>
+								<li>
+									<a href="https://twitter.com/shuvorobin"><i class="fa fa-twitter"></i></a>
+									
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
